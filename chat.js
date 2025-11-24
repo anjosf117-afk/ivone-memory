c6c52631d81369ea6a557a1aff30d7fe114bad08
anjosf117-afk <anjosf117@gmail.com>
2025-11-23 19:33:13 -0500

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,6 +1,6 @@
 
 export default async function handler(req, res) {
-  // Simple in-memory memory (resets each deploy)
+  // Simple in-memory memory
   global.memory = global.memory || [];
 
   if (req.method === "POST") {
