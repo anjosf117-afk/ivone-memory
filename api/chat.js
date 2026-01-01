6dd8e85d8e072594a4d08972c660e415e0aae4d7
anjosf117-afk <anjosf117@gmail.com>
2025-12-31 19:28:07 -0500

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
       conversationHistory.shift();
     }
 
-    const MAX_MESSAGES = 8;
+    const MAX_MESSAGES = 2;
 
 if (conversationHistory.length >= MAX_MESSAGES) {
   return res.status(200).json({
